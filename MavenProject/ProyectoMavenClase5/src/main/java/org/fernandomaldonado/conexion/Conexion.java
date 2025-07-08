c4c0e06386018c6723c5bd36be1dc930921a28ed
FernanMaldonado <fmaldonado-2024021@kinal.edu.gt>
2025-07-08 11:43:09 -0600

    Se creo a la conexion con la base de datos

---
+++ MavenProject/ProyectoMavenClase5/src/main/java/org/fernandomaldonado/conexion/Conexion.java
@@ -0,0 +1,50 @@
+package org.fernandomaldonado.conexion;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+ 
+/**
+*
+* @author informatica
+*/
+public class Conexion {
+    private static Conexion instancia;
+    private Connection conexion; 
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/VentaDeComida?user=false";
+    private static final String USER = "quinto";
+    private static final String PASSWORD = "admin";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    public Conexion(){
+        conectar();
+    }
+    public void conectar(){
+        try {
+            Class.forName(DRIVER).newInstance();
+            conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexion realizada con exito");
+        } catch (ClassNotFoundException | InstantiationException |
+                IllegalAccessException | SQLException ex) { 
+            System.out.println("Error al conectar");
+        }
+    }
+ 
+    public static Conexion getInstancia() {
+        if (instancia ==null) {
+            instancia = new Conexion();
+        }
+        return instancia;
+    }
+ 
+    public Connection getConexion() {
+        try {
+            if (conexion ==null || conexion.isClosed()) {
+                conectar();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conexion;
+    }    
+}  
