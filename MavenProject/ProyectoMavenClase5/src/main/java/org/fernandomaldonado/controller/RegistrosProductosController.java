4fcb2f0e12af05003b7a3fc2b20dda41934d7873
GiancarloCordova <gcordova-2024016@kinal.edu.gt>
2025-07-08 20:43:51 -0600

    Se incorporo el cambio de escena entre las vistas

--- MavenProject/ProyectoMavenClase5/src/main/java/org/fernandomaldonado/controller/RegistrosProductosController.java
+++ MavenProject/ProyectoMavenClase5/src/main/java/org/fernandomaldonado/controller/RegistrosProductosController.java
@@ -6,7 +6,11 @@ package org.fernandomaldonado.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import org.fernandomaldonado.system.Main;
 
 /**
  * FXML Controller class
@@ -14,13 +18,30 @@ import javafx.fxml.Initializable;
  * @author acord
  */
 public class RegistrosProductosController implements Initializable {
+    private Main principal;
 
+@FXML private Button btnRegresar;
+
+    public void setPrincipal(Main principal) {
+        this.principal = principal;
+    }
+    
+    
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }   
+    
+    @FXML
+    public void clickRP(ActionEvent evento) {
+        if (evento.getSource() == btnRegresar) {
+            System.out.println("Regresando...");
+            principal.Inicio();
+        }
+    }
     
 }
